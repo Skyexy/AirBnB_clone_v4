a6844d77fd87ecedfab947bb0f0d2aaa9777d8b8
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-30 14:01:47 +0100

    Create 3-hbnb.js

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,29 @@
+var dict = {};
+$(document).ready(function() {
+  $('input[type=checkbox]').change(
+    function(){
+      if (this.checked) {
+        dict[this.data-id] = this.data-name;
+      }
+      else{
+        delete dict[this.data-id];
+      }
+    });
+  if (Object.keys(dict).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+  }
+  else{
+    $('div.amenities h4').text(Object.values(dict).join(', '));
+  }
+  $.get("http://0.0.0.0:5001/api/v1/places_search/", function(data, status){
+    if(xhr.status==200){
+      $('div#api_status').addClass('available');
+    }
+    else{
+       $('div#api_status').removeClass('available')
+    }
+  });
+  $.post( "ajax/test.html", function( data ) {
+    $( ".result" ).html( data );
+  });
+});
