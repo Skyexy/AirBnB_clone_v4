71bdf3145fe23d0fd70af8735922dfa1632eb856
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-30 14:23:34 +0100

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       }
     });
   if (Object.keys(dict).length === 0) {
-      $('div.amenities h4').html('&nbsp');
+    $('div.amenities h4').html('&nbsp');
   }
   else{
     $('div.amenities h4').text(Object.values(dict).join(', '));
@@ -20,16 +20,15 @@ $(document).ready(function() {
       $('div#api_status').addClass('available');
     }
     else{
-       $('div#api_status').removeClass('available')
+      $('div#api_status').removeClass('available')
     }
   });
-  $.ajax({
-            type: 'POST',
-            url: 'http://0.0.0.0:5001/api/v1/places_search/',
-            data: curl "http://0.0.0.0:5001/api/v1/places_search" -XPOST -H "Content-Type: application/json" -d '{}',
-            contentType: "application/json",
-            traditional: true,
-            success: function (data) {
-            }
-        });
+  $.ajax ({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: curl "http://0.0.0.0:5001/api/v1/places_search" -XPOST -H "Content-Type: application/json" -d '{}',
+    contentType: "application/json",
+    traditional: true,
+    success: function (data) {}
+  });
 });
